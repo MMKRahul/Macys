4ff1ef7712f2bf09fc8c612e663e5d830f2b2ccc
MMKRahul <48232594+MMKRahul@users.noreply.github.com>
2019-05-21 01:19:40 -0500

    Update MacysEmailPass.java

--- src/test/MacysEmailPass.java
+++ src/test/MacysEmailPass.java
@@ -10,16 +10,16 @@ import values.MacysValues;
 
 public class MacysEmailPass extends Config{
 	
-	MacysLocators 	fbl = new MacysLocators();
-	MacysValues			fbv = new MacysValues();
+	MacysLocators 	mcl = new MacysLocators();
+	MacysValues			mcv = new MacysValues();
 
 	@Test // test annotion - without this annotation, you can't execute any tes
 	public void testFacebookEmailPass(){
 
 		// Enter firstName
-		driver.findElement(By.xpath(fbl.email)).sendKeys(fbv.emailVal);
+		driver.findElement(By.xpath(mcl.email)).sendKeys(mcv.emailVal);
 		// Enter lastName
-		driver.findElement(By.xpath(fbl.pass)).sendKeys(fbv.passVal);
+		driver.findElement(By.xpath(mcl.pass)).sendKeys(mcv.passVal);
 	}
 
 }
